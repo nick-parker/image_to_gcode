543a125220892ff75de67f24f55114a3dfe156ec
nick-parker <github@nicholasparker.org>
2014-07-12 17:30:41 -0500

    changes I'll revert

---
+++ display.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+def display(filename,width,height):
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    #Setup Command line arguments
+    parser = argparse.ArgumentParser(prog="display.py",
+                                     usage="%(prog)s input...",
+                                     description="Display gcode with pyplot"
+                                     )
+    
